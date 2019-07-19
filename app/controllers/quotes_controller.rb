538d64e3f46229dcc2ca3c33bc7dd6c8411ebc7b
Rosy Arreola <rosy.arreola@yahoo.com>
2019-07-19 19:28:54 +0000

    Fixing up links

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,8 +1,22 @@
 class QuotesController < ApplicationController
+ 
   def index
     @quote = Quote.order("RANDOM()").first
   end
+  
   def new
     @quote = Quote.new
   end
+
+  def create
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
+
 end
