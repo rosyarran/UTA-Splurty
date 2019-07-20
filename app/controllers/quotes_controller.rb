3b8ad25c53cdd5dbda0ef288bba26d3bc4434444
Rosy Arreola <rosy.arreola@yahoo.com>
2019-07-20 05:36:29 +0000

    About

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,10 +3,6 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
   end
-  
-  def new
-    @quote = Quote.new
-  end
 
   def create
     @quote = Quote.create(quote_params)
